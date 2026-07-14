233449e56f27bd600fbb0953ad38e79b42fa327f
tamsi <165705077+TamaraStankovska1@users.noreply.github.com>
2026-07-14 15:09:37 +0200

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+
+        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        char[] chars = s.toCharArray();
+        char[] reversedchars = new char[chars.length];
+
+        for (int i = chars.length - 1; i>=0; i--) {
+            reversedchars[i] = chars[chars.length - 1 -i];
+        }
+        if (Arrays.equals(chars, reversedchars)) {
+            return true;
+        }
+        return false;
+    }
+}
