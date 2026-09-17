9f8c2788b877b3b09b4ef98e0190224e7e3f0640
tamsi <165705077+TamaraStankovska1@users.noreply.github.com>
2026-09-17 18:35:52 +0300

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] sortedS = s.toCharArray();
+        char[] sortedT = t.toCharArray();
+        Arrays.sort(sortedS);
+        Arrays.sort(sortedT);
+
+       if (Arrays.equals(sortedS, sortedT)) {
+            return true;
+        }
+        return false;
+    }
+}
